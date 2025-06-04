748e925a2e63723602ef42920e74d275e750ac87
8729837489327 <jayreynaga20082@gmail.com>
2025-06-03 18:21:40 -0700

    update !

---
+++ eternityz/main.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const video = document.getElementById("background");
+  const container = document.getElementById("container");
+  const volumeControl = document.getElementById("volume-control");
+  const overlay = document.getElementById("clickOverlay");
+
+  overlay.addEventListener("click", () => {
+    video.muted = false;
+    video.play();
+    video.style.filter = "none";
+
+    overlay.style.display = "none";
+
+    // Fade in container and volume control by adding "visible" class
+    container.classList.add("visible");
+    volumeControl.classList.add("visible");
+  });
+
+  document
+    .getElementById("volume-slider")
+    .addEventListener("input", (event) => {
+      video.volume = event.target.value;
+    });
+});
