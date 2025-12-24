98da11a8e6f9ab622efdf0d7cc5c4c21fded3c0f
8729837489327 <jayreynaga20082@gmail.com>
2025-12-24 15:20:45 -0800

    22

---
+++ yourstruly/main.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const video = document.getElementById("background");
+  const music = document.getElementById("background-music");
+  const container = document.getElementById("container");
+  const volumeControl = document.getElementById("volume-control");
+  const overlay = document.getElementById("clickOverlay");
+
+  // Detect mobile device
+  const isMobile = /iPhone|iPad|iPod|Android|webOS|BlackBerry|Windows Phone/i.test(navigator.userAgent);
+  if (isMobile) {
+    volumeControl.style.opacity = "0";
+  }
+
+  overlay.addEventListener("click", () => {
+    video.muted = false;
+    video.play();
+    // music.play();
+    music.volume = 0;
+    video.volume = 1;
+    video.style.filter = "none";
+    video.style.opacity = "1";
+    overlay.style.display = "none";
+
+    container.classList.add("visible");
+
+    // Only show volume control if not on mobile
+    if (!isMobile) {
+      muteBtn.classList.add("visible");
+    }
+  });
+
+  // MUTE BUTTON
+  const muteBtn = document.getElementById("mute-btn");
+  muteBtn.addEventListener("click", () => {
+    const isMuted = music.muted;
+
+    music.muted = !isMuted;
+    muteBtn.textContent = isMuted ? "🔊" : "🔇";
+  });
+});
